3610ffc3ca025eeca25744454ae377ab544e2482
Lydia-09 <pasky0901@gmail.com>
2022-04-21 18:16:10 +0800

    feat: add express-session & user login feature

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session')
 const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const flash = require('connect-flash')
@@ -11,6 +12,7 @@ if (process.env.NODE_ENV !== 'production') {
 // 引用路由器
 const routes = require('./routes')
 
+const usePassport = require('./config/passport')
 // 引用 mongoose 連線設定檔
 require('./config/mongoose')
 
@@ -19,10 +21,23 @@ const app = express()
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+usePassport(app)
 app.use(flash())
+app.use((req, res, next) => {
+  // console.log(req.user)
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 
 // 將 request 導入路由器
 app.use(routes)
